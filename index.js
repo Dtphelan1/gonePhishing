2368ebf475d285828fb47d517004e040fc2a1f24
root <root@localhost.localdomain>
2015-12-13 02:10:20 -0500

    changes to index.js

--- index.js
+++ index.js
@@ -58,5 +58,9 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 
 module.exports = app;
