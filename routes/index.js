6e71cee74a7d6feabf398ed72e8787bcc197f9f2
root <root@localhost.localdomain>
2015-12-14 22:46:50 -0500

    working on ip module; console logging added

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+    console.log(ip);
     var md5 = crypto.createHash('md5').update(ip).digest("hex");
     var array = fs.readFileSync('ips.txt').toString().split("\n");
     var contains = false;
