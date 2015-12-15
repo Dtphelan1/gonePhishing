175cf5f5ce3d4b477743dfe5208942b5da5ef5a5
root <root@localhost.localdomain>
2015-12-14 23:12:43 -0500

    scrapped ip idea, relying on good will of victims -- wish me luck

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
-    console.log(ip);
-    var md5 = crypto.createHash('md5').update(ip).digest("hex");
-    var array = fs.readFileSync('ips.txt').toString().split("\n");
-    var contains = false;
     
-    for(i in array) {
-	if (i == md5) {
-	    contains = true;
-	    break;
-	}
-    }
-    if (!contains) {
-	fs.writeFileSync('ips.txt', md5 + "\n");
-    }
     
     res.render('index', { title: '' });
 });
